12112f19e4335e58c66d3533d370c01c9481176a
Rajesh <160474606+rajeshmestha@users.noreply.github.com>
2025-06-01 17:45:46 +0530

    Create priority_encoder.sv

---
+++ src/priority_encoder.sv
@@ -0,0 +1,19 @@
+module priority_encoder_4to2 (
+    input  [3:0] in,
+    output reg [1:0] out,
+    output reg valid
+);
+    always @(*) begin
+        valid = 1'b1;
+        casez (in)
+            4'b1???: out = 2'b11;
+            4'b01??: out = 2'b10;
+            4'b001?: out = 2'b01;
+            4'b0001: out = 2'b00;
+            default: begin
+                out = 2'b00;
+                valid = 1'b0;
+            end
+        endcase
+    end
+endmodule
